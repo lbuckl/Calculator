08de8d22f57c1d4254438ef5f7c86ce91d7e2b23
lbuckl <vadimufastr@gmail.com>
2022-05-08 16:08:32 +0300

    Add functional in buttons and textView

--- app/src/main/java/com/vados/calculator/MainActivity.java
+++ app/src/main/java/com/vados/calculator/MainActivity.java
@@ -10,12 +10,17 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView_result;
     private TextView textView_info;
+    private Button buttons[] = new Button[15];
+    private String result = "";
+    private Button button_result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialization();
+        clickListener();
 
 
         //textView_info.setText("Привет");
@@ -23,25 +28,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialization(){
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button5 = findViewById(R.id.button5);
-        Button button6 = findViewById(R.id.button6);
-        Button button7 = findViewById(R.id.button7);
-        Button button8 = findViewById(R.id.button8);
-        Button button9 = findViewById(R.id.button9);
-        Button button0 = findViewById(R.id.button0);
-        Button button_point = findViewById(R.id.button_point);
-        Button button_result = findViewById(R.id.button_result);
-        Button button_plus = findViewById(R.id.button_plus);
-        Button button_minus = findViewById(R.id.button_minus);
-        Button button_mult = findViewById(R.id.button_mult);
-        Button button_dif = findViewById(R.id.button_dif);
+        buttons[0] = findViewById(R.id.button0);
+        buttons[1] = findViewById(R.id.button1);
+        buttons[2] = findViewById(R.id.button2);
+        buttons[3] = findViewById(R.id.button3);
+        buttons[4] = findViewById(R.id.button4);
+        buttons[5] = findViewById(R.id.button5);
+        buttons[6] = findViewById(R.id.button6);
+        buttons[7] = findViewById(R.id.button7);
+        buttons[8] = findViewById(R.id.button8);
+        buttons[9] = findViewById(R.id.button9);
+        buttons[10] = findViewById(R.id.button_point);
+        buttons[11] = findViewById(R.id.button_plus);
+        buttons[12] = findViewById(R.id.button_minus);
+        buttons[13] = findViewById(R.id.button_mult);
+        buttons[14] = findViewById(R.id.button_dif);
+        button_result = findViewById(R.id.button_result);
 
         textView_info = findViewById(R.id.textView_info);
         textView_info = findViewById(R.id.textView_result);
 
+    }
+
+    public void clickListener(){
+        //запускаем подслушку на основные кнопки
+        for (Button e:buttons) {
+            e.setOnClickListener(v -> {
+                result += e.getText();
+                printEnters();
+            });
+        }
+
+        button_result.setOnClickListener(v -> {
+            String[] nums = result.split("\\+|\\-|\\*|\\/");
+
+        });
+    }
+
+    private void printEnters(){
+        textView_info.setText(result);
     }
 }
